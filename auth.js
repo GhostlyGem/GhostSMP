8167e1ed6ef4e08072524605c005c61206942b8e
GhostlyGem <prestonclement566@gmail.com>
2026-03-13 09:39:21 -0500

    Add Google login functionality with Firebase
    Implement Google authentication using Firebase in auth.js.

--- auth.js
+++ auth.js
@@ -1,1 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+const firebaseConfig = {
+
+apiKey: "PASTE_KEY_HERE",
+authDomain: "PASTE_DOMAIN",
+projectId: "PASTE_ID",
+storageBucket: "PASTE_BUCKET",
+messagingSenderId: "PASTE_ID",
+appId: "PASTE_APP_ID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const provider = new GoogleAuthProvider();
+
+const loginButton = document.getElementById("google-login");
+
+if(loginButton){
+
+loginButton.addEventListener("click", () => {
+
+signInWithPopup(auth, provider)
+.then((result) => {
+
+alert("Logged in as " + result.user.displayName);
+
+})
+.catch((error) => {
+
+console.error(error);
+
+});
+
+});
+
+}
