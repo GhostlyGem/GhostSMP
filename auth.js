8eafdb4ee809dddb3a9c4d6b9f793ed3f925bbc2
GhostlyGem <prestonclement566@gmail.com>
2026-03-14 06:03:02 -0500

    Update Firebase config with real values
    Updated Firebase configuration with actual credentials.

--- auth.js
+++ auth.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstati
 
 const firebaseConfig = {
 
-apiKey: "PASTE_KEY_HERE",
-authDomain: "PASTE_DOMAIN",
-projectId: "PASTE_ID",
-storageBucket: "PASTE_BUCKET",
-messagingSenderId: "PASTE_ID",
+apiKey: "AIzaSyC9bCU2pRu0VGi0chBDdupYPSo5FxPSimo",
+authDomain: "ghostsmp-bf0a3.firebaseapp.com",
+projectId: "ghostsmp-bf0a3",
+storageBucket: "ghostsmp-bf0a3.firebasestorage.app",
+messagingSenderId: "415275850062",
 appId: "PASTE_APP_ID"
-
+1:415275850062:web:c64aa3147dec2212a7661f
 };
 
 const app = initializeApp(firebaseConfig);
