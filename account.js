e62539cfa4aff60954d8a4bb4a3cef6ebab0952c
GhostlyGem <prestonclement566@gmail.com>
2026-03-15 17:05:37 -0500

    Create account.js

---
+++ account.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getAuth,
+onAuthStateChanged,
+updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+getFirestore,
+doc,
+updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyC9bCU2pRu0VGi0chBDdupYPSo5FxPSimo",
+authDomain: "ghostsmp-bf0a3.firebaseapp.com",
+projectId: "ghostsmp-bf0a3",
+storageBucket: "ghostsmp-bf0a3.firebasestorage.app",
+messagingSenderId: "415275850062",
+appId: "1:415275850062:web:c64aa3147dec2212a7661f"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const input = document.getElementById("display-name");
+const saveBtn = document.getElementById("save-name");
+
+onAuthStateChanged(auth,(user)=>{
+
+if(!user) return;
+
+input.value = user.displayName || "";
+
+saveBtn.onclick = async ()=>{
+
+const newName = input.value;
+
+await updateProfile(user,{
+displayName:newName
+});
+
+await updateDoc(doc(db,"users",user.uid),{
+name:newName
+});
+
+await updateDoc(doc(db,"websiteOnline",user.uid),{
+name:newName
+});
+
+alert("Name updated!");
+
+};
+
+});
